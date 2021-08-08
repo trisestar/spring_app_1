f7e990357732c727cd03fe0de610f9676ed0803f
trisestar <a21plusartyom@gmail.com>
2021-08-08 17:57:36 +0300

    Initial commit

---
+++ src/main/java/com/meshkov/test_task/controllers/MainController.java
@@ -0,0 +1,13 @@
+package com.meshkov.test_task.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping
+    public String hello(){
+        return "hello";
+    }
+}
